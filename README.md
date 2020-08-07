3c4dec061e54ae0ce765b4fc612605fd4f5f7681
Miouns <goodgamersz665@gmail.com>
2020-08-07 17:15:03 +0700

    backup from bladesquad

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# BladeSquad
-official BladeSquad repository
+# Hmmm
---
+++ commands/dev/dm.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  if(client.config.owners.includes(message.author.id)) {
+  
+  const user = message.mentions.users.first() || client.users.cache.get(args[0])
+  
+  
+  let embed = new Discord.MessageEmbed()
+      .setDescription(`Succesfully send DMs to <@${user.id}>`)
+      .setThumbnail(`${client.user.displayAvatarURL()}`)
+  // message.author.send()
+  try {
+    if(user) {
+      client.users.cache.get(user.id).send(args.join(" "))
+      message.channel.send(embed);
+  } else {
+    client.users.cache.get(args[0]).send(args.join(" "))
+    message.channel.send(embed)
+  }
+  } catch (e) {
+    message.channel.send("Please Mention a user or input with id user", e);
+  }
+  }
+}
+exports.help = {
+  name: "dm",
+  description: "Send DMs message"
+}
+
+exports.conf = {
+  aliases: [],
+  cooldown: 3
+}
+
